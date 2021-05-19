d13d1dfc2971a19cb80bc062af5d291d63d48a7b
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-19 19:05:59 +0900

    Fix logo path

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -2,6 +2,20 @@
   <img class="VuetifyLogo" alt="Vuetify Logo" src="/vuetify-logo.svg" />
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+const environment = process.env.NODE_ENV || 'local'
+const env = require(`./env/${environment}.ts`)
+
+export default Vue.extend({
+  data() {
+    return {
+      vuetifyLogo: require(`${env.BASE_URL}vuetify-logo.svg`),
+    }
+  },
+})
+</script>
+
 <style>
 .VuetifyLogo {
   height: 180px;
