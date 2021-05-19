372d9eb64154cfacc0ca10afd22cdc2940595796
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-19 17:22:17 +0900

    Modify GitHub Actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ export default {
   },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/doc-y/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
