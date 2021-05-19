cbea62bdc39bdc017b6463231b22f738f7ffe9a8
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-19 15:04:35 +0900

    Deleted right menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,27 +37,15 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
+      <NuxtLink to="/">Home</NuxtLink>
     </v-footer>
   </v-app>
 </template>
@@ -82,9 +70,7 @@ export default {
         },
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'RUTILEA GUI GROUP',
     }
   },
 }
