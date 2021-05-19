373d66984b8ac90178b6f5abfb9f705ad65da89c
tuckKome <takuya.kometan@gmail.com>
2021-05-19 09:52:39 +0000

    Deploying to gh-pages from  @ b32ca8bd7ad07635f8aadfaaab2f40a1b14a21b2 🚀

--- _nuxt/static/1621416282/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdoc-y\u002F_nuxt\u002Fstatic\u002F1621416282",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdoc-y\u002F",assetsPath:"\u002Fdoc-y\u002F_nuxt\u002F",cdnURL:a}}}}(null));
