b32ca8bd7ad07635f8aadfaaab2f40a1b14a21b2
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-19 18:51:03 +0900

    generate docs

---
+++ docs/_nuxt/static/1621417770/pm/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdoc-y\u002F_nuxt\u002Fstatic\u002F1621417770",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpm",config:{_app:{basePath:"\u002Fdoc-y\u002F",assetsPath:"\u002Fdoc-y\u002F_nuxt\u002F",cdnURL:a}}}}(null));
