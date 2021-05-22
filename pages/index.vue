0e85b91c5167035053043bd308f6bd2358cfbe2f
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-22 15:54:58 +0900

    Delete Vuetify logo & Add PM card

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,14 @@
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
         <logo />
-        <vuetify-logo />
       </div>
-      <v-card>
+      <v-card class="ma-2">
         <v-card-title class="headline"> RUTILEA GUI GROUP </v-card-title>
         <v-card-text>
           <p>
             Nuxt.js + Vuetify + GitHub Pagesで作るGUI班のドキュメントです。
             自由に編集しましょう。 Nuxt.jsとは、Vue.jsのためのServer Side
             Generateフレームワークです。
-          </p>
-          <p>
-            <NuxtLink to="/pm">プロジェクト管理について</NuxtLink>
           </p>
           <hr class="my-3" />
           <a
@@ -38,18 +34,27 @@
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
         </v-card-actions>
       </v-card>
+      <v-card class="ma-2" :ripple="{ center: false }" nuxt>
+        <v-card-title class="headline"> プロジェクト管理について </v-card-title>
+        <v-card-text>
+          米谷がはじめてPM（プロジェクトマネージャー）をやって気がついたことをシェアします。
+          やってよかったこと、改善できることをまとめていきます。
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" nuxt to="/pm">プロジェクト管理へ</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
     Logo,
-    VuetifyLogo,
   },
 }
 </script>
