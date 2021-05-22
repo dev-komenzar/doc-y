c048e33db6f57f17417498dafbe8d10744fb7000
こめたに・たくや <takuya.kometan@gmail.com>
2021-05-22 17:24:10 +0900

    Add meta tag. Avoid Google

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'robots', content: 'noindex' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: `${env.BASE_URL}favicon.ico` },
