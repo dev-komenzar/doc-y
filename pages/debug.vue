0b451b318bf30194783085c3e19b2d374d46730a
shimmar <riku2000ff15@gmail.com>
2021-05-24 15:52:27 +0900

    デバッグページ加筆

--- pages/debug.vue
+++ pages/debug.vue
@@ -6,7 +6,7 @@
           >Vue + Electron デバッグまとめ</v-card-title
         >
         <v-card-text>
-          <h2 class="mb-2">Javascriptの組み込みデバッガー</h2>
+          <h3 class="mb-2">Javascriptの組み込みデバッガー</h3>
           <p>
             コード内にdebuggerステートメントを記入して動作を確認する基本的な方法。
           </p>
@@ -15,7 +15,18 @@
               >組み込みデバッガーの使い方</a
             >
           </p>
-          <h2 class="mb-2"></h2>
+          <h3 class="mb-2">Electronでのデバッグ</h3>
+          <p>
+            <a href="https://final.hateblo.jp/entry/electron-debug"
+              >Electron with Vue.js でのデバッグのやり方をまとめる</a
+            >
+          </p>
+          <h3 class="mb-2">consoleオブジェクトのメソッド</h3>
+          <p>
+            <a href="https://qiita.com/baby-degu/items/1046763163bc794870ea"
+              >JavaScriptでconsole.log()を使うのはやめよう</a
+            >
+          </p>
         </v-card-text>
       </v-card>
     </v-col>
